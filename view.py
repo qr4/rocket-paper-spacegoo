3a41a5e1e8006a9c148057d879084423ed1f4294
cupe <cupe-bitbucket@erleuchtet.org>
2013-05-26 20:36:32 +0200

    viewer ist jetzt nur noch so medium shitty

--- view.py
+++ view.py
@@ -20,7 +20,7 @@ def load_texture(filename):
         GL_TEXTURE_2D, 0, GL_RGBA, width, height,
         0, GL_RGBA, GL_UNSIGNED_BYTE, image.tostring()
     )
-    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR)
+    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR)
     glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR_MIPMAP_LINEAR)
     glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
     glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
@@ -49,14 +49,22 @@ def draw_image():
 
 def color_for_owner(owner):
     if owner == 0:
-        glColor4f(1, 1, 1, 1)
+        glColor4f(0.6, 0.6, 0.6, 1)
     elif owner == 1:
         glColor4f(1, 0.5, 0.5, 1)
     else:
         glColor4f(0.5, 1, 0.5, 1)
         
 def scale_for_fleetsize(size):
-    return math.sqrt(size)/3.0
+    return math.log(size+1)/2.0
+
+class Fleet():
+    def __init__(self,ships,position,direction,player):
+        self.ships = ships
+        self.direction = direction
+        self.position = position
+        self.player = player
+    
 
 def update(state):
     window.dispatch_events()
@@ -73,7 +81,7 @@ def update(state):
     glPushMatrix()
 
     glTranslatef(1024/2, 768/2, 0)
-    glScalef(30, 30, 1)
+    glScalef(20, 20, 1)
 
     glColor4f(1,0,0,1)
 
@@ -90,17 +98,18 @@ def update(state):
         draw_image()
         glPopMatrix()
 
-    glBindTexture(GL_TEXTURE_2D, ship_tex)
-    for planet in state['planets']:
-        glPushMatrix()
-        glTranslatef(planet['x'], planet['y'], 0)
-        size = math.sqrt(sum(planet['ships']))/3
-        glScalef(size, size, 1)
-        glColor4f(1,1,1,1)
-        draw_image()
-        glPopMatrix()
 
     ship_textures = [ship_tex,goo_tex,asteroid_tex]
+    fleets= []
+    
+    #glBindTexture(GL_TEXTURE_2D, ship_tex)
+    for planet in state['planets']:
+        pos = vector.Point(planet['x'], planet['y'])
+        direction = vector.Point(1,0)
+        fleet = Fleet(planet['ships'], pos, direction, planet['owner_id'] )
+        fleets.append(fleet)
+        
+
     
     for fleet in state['fleets']:
         origin = planet_by_id[fleet['origin']]
@@ -110,15 +119,24 @@ def update(state):
 
         time_remaining = fleet['eta'] - state['round']
         direction = vector.Normalize(origin - target)
-        angle = vector.Angle(direction)
+        
         pos = target + direction * time_remaining
+        owner = fleet['owner_id']
         ships = fleet['ships']
+        fleets.append(Fleet(ships,pos,direction,owner))
+        
+        
+    for fleet in fleets:
+        owner = fleet.player
+        ships = fleet.ships
+        pos = fleet.position
+        angle = vector.Angle(fleet.direction)
         size = scale_for_fleetsize(sum(ships))
         glPushMatrix()
         glTranslatef(pos.x, pos.y, 0)
         glRotatef(angle+180,0,0,1)
         #glScalef(size, size, 1)
-        color_for_owner(fleet['owner_id'])
+        color_for_owner(owner)
         for i,ship in enumerate(ships):
             glPushMatrix()
             glRotatef(i*120,0,0,1)
