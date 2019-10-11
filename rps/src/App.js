0e40bc91b010cfe6336ead6c5a5480f50a251b3f
Pascal Ernst <linucc@linu.cc>
2019-10-11 21:44:30 +0100

    Add sounds

--- rps/src/App.js
+++ rps/src/App.js
@@ -1,30 +1,65 @@
-import React from 'react';
 import './App.css';
-import {Route, BrowserRouter as Router, Switch} from "react-router-dom";
-import { Arwes, ThemeProvider, createTheme, Header, Heading, Words, Logo } from '@arwes/arwes';
+
+import {
+  Arwes,
+  ThemeProvider,
+  createTheme,
+  Header,
+  Heading,
+  Logo,
+} from '@arwes/arwes';
+import {Howl} from 'howler';
+import {Route, BrowserRouter as Router, Switch} from 'react-router-dom';
+import React from 'react';
+
+import {SoundsProvider} from '@arwes/sounds';
+
+import { SoundWords } from './components';
+
+const players = {
+    ask: new Howl({src: [`${process.env.PUBLIC_URL}/sounds/ask.mp3`]}),
+    click: new Howl({src: [`${process.env.PUBLIC_URL}/sounds/click.mp3`]}),
+    deploy: new Howl({src: [`${process.env.PUBLIC_URL}/sounds/deploy.mp3`]}),
+    error: new Howl({src: [`${process.env.PUBLIC_URL}/sounds/error.mp3`]}),
+    information: new Howl({
+        src: [`${process.env.PUBLIC_URL}/sounds/information.mp3`],
+    }),
+    typing: new Howl({src: [`${process.env.PUBLIC_URL}/sounds/typing.mp3`]}),
+    warning: new Howl({src: [`${process.env.PUBLIC_URL}/sounds/warning.mp3`]}),
+};
+const audio = {
+    mute: false,
+};
 
 function Home() {
-    return "";
+    return '';
 }
 
 function App() {
     return (
         <ThemeProvider theme={createTheme()}>
-            <Arwes animate background='/background-large.jpg' pattern='/glow.png'>
-                <Router>
-                    <Header animate>
-                        <Heading node="span">
-                            <Logo animate size={50}/>
-                            <Words animate style={{ marginLeft: '5px'}}>Rock Paper Scissors</Words>
-                        </Heading>
-                    </Header>
-                    <Switch>
-                        <Route path="/">
-                            <Home />
-                        </Route>
-                    </Switch>
-                </Router>
-            </Arwes>
+            <SoundsProvider players={players} audio={audio}>
+                <Arwes
+                    animate
+                    background="/background-large.jpg"
+                    pattern="/glow.png">
+                    <Router>
+                        <Header animate>
+                            <Heading node="span">
+                                <Logo animate size={50} />
+                                <SoundWords animate style={{marginLeft: '5px'}}>
+                                    Rock Paper Scissors
+                                </SoundWords>
+                            </Heading>
+                        </Header>
+                        <Switch>
+                            <Route path="/">
+                                <Home />
+                            </Route>
+                        </Switch>
+                    </Router>
+                </Arwes>
+            </SoundsProvider>
         </ThemeProvider>
     );
 }
---
+++ rps/src/components/index.js
@@ -0,0 +1,10 @@
+import {Button, Words} from '@arwes/arwes';
+import React from 'react';
+
+import {withSounds} from '@arwes/sounds';
+
+const withCoolSounds = () => InnerComp =>
+    withSounds()(props => <InnerComp {...props} sounds={props.players} />);
+
+export const SoundButton = withCoolSounds()(Button);
+export const SoundWords = withCoolSounds()(Words);
